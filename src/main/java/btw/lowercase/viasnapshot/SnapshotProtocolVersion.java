d633b9c39dfcd8dfc3d0380542005b943f6aa276
lowercasebtw <jejojora2@gmail.com>
2024-10-15 18:03:45 -0400

    Update to ViaProxy 3.3.4-SNAPSHOT

--- src/main/java/btw/lowercase/viasnapshot/SnapshotProtocolVersion.java
+++ src/main/java/btw/lowercase/viasnapshot/SnapshotProtocolVersion.java
@@ -1,7 +1,14 @@
 package btw.lowercase.viasnapshot;
 
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import com.viaversion.viaversion.protocol.RedirectProtocolVersion;
 
 public class SnapshotProtocolVersion {
-    public static final ProtocolVersion v15w31a = ProtocolVersion.register(49, "15w31a (Client Only)");
+    public static final ProtocolVersion v15w31a = registerSnapshot(49, "15w31a (Client Only)", ProtocolVersion.v1_8);
+
+    private static ProtocolVersion registerSnapshot(final int version, final String name, final ProtocolVersion origin) {
+        final ProtocolVersion protocolVersion = new RedirectProtocolVersion(version, name, origin);
+        ProtocolVersion.register(protocolVersion);
+        return protocolVersion;
+    }
 }
